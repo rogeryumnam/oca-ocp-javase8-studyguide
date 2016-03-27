79ce3aabe059eac3bf1557e4e65953851bac30c3
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-03-26 20:48:40 -0400

    Chapter 1 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch01/e05_classes_vs_files/Animal.java
@@ -0,0 +1,12 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch01.e05_classes_vs_files;
+
+/**
+ * Chapter 1: Java Building Blocks<br>
+ * Understanding the Java Class Structure<br>
+ * Classes vs. Files - page 5
+ * <p>
+ * Most of the time, each Java class is defined in its own .java file.  Java classes do not have to be public
+ */
+class Animal {
+	String name;
+}
