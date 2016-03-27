1e1f598f840e27cb761cf64ff8092bd10ff42753
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-03-26 20:40:22 -0400

    Chapter 1 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch01/e01_simplest_class/Animal.java
@@ -0,0 +1,10 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch01.e01_simplest_class;
+
+/**
+ * Chapter 1: Java Building Blocks<br>
+ * Understanding the Java Class Structure<br>
+ * Fields and Methods - page 3
+ * <p>
+ * This is the simplest Java class that can be written
+ */
+class Animal {}
