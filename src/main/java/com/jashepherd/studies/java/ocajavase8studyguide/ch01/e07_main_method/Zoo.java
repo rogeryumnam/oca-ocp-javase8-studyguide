3976f6eac261af26604aa01303b63289e7019ef7
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-03-27 15:10:00 -0400

    Chapter 1 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch01/e07_main_method/Zoo.java
@@ -0,0 +1,17 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch01.e07_main_method;
+
+/**
+ * Chapter 1: Java Building Blocks<br>
+ * Understanding the Java Class Structure<br>
+ * Writing a main() Method - page 6
+ * <p>
+ * This class illustrates the most common variant of writing the "special" main() method<br>
+ * Other variants include:<br>
+ *    <code>public static void main(String args[]);</code>          <br>
+ *    <code>public static void main(String... args);</code>         <br>
+ *    <code>public static void main(String[] somethingElse);</code> <br>
+ *    <code>static public void main(String bang_a_gong[]);</code>
+ */
+public class Zoo {
+	public static void main(String[] args) {}
+}
