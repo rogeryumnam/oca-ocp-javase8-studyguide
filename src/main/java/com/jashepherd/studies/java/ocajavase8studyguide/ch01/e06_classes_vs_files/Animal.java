e577e595f97bafc7a42b83dcad0b26d821f91054
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-03-26 20:50:29 -0400

    Chapter 1 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch01/e06_classes_vs_files/Animal.java
@@ -0,0 +1,14 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch01.e06_classes_vs_files;
+
+/**
+ * Chapter 1: Java Building Blocks<br>
+ * Understanding the Java Class Structure<br>
+ * Classes vs. Files - page 5
+ * <p>
+ * You can put two classes in the same file.  When you do, at most one of the classes in the file is allowed to be public and the file
+ * name must match the name of the public class.
+ */
+public class Animal {
+	private String name;
+}
+class Animal2 {}
