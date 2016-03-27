092429039cb9e46af5418991ee800056d01adcd6
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-03-27 15:53:48 -0400

    Chapter 1 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch01/e11_import_wildcards/ImportExample.java
@@ -0,0 +1,20 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch01.e11_import_wildcards;
+
+import java.util.*;  // imports java.util.Random among other things
+
+/**
+ * Chapter 1: Java Building Blocks<br/>
+ * Understanding Package Declarations and Imports<br/>
+ * Wildcards - page 10
+ * <p>
+ * Classes in the same package are often imported together. You can use a shortcut to import all
+ * the classes in a package. The * is a wildcard that matches all classes in the package. Every
+ * class in the java.util package is available to this program when Java compiles it. It doesn't
+ * import child packages, fields, or methods; it imports only classes.
+ */
+public class ImportExample {
+	public static void main(String[] args) {
+		Random r = new Random();
+		System.out.println(r.nextInt(10));
+	}
+}
