836bc339751738e019a6d0c1d57050d8bfa7620f
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-03-28 23:42:52 -0400

    Chapter 1 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch01/e17_constructors/Chick.java
@@ -0,0 +1,20 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch01.e17_constructors;
+
+/**
+ * Chapter 1: Java Building Blocks<br/>
+ * Creating Objects<br/>
+ * Constructors - page 17
+ * <p>
+ * There are two key points to note about the constructor: the name of the constructor
+ * matches the name of the class, and there’s no return type.
+ * <p>
+ * When you see a method name beginning with a capital letter and having a return type,
+ * pay special attention to it. It is not a constructor since there’s a return type. It’s a regular
+ * method that won’t be called when you write new Chick()<br>
+ * <code>public void Chick() { } // NOT A CONSTRUCTOR</code>
+ */
+public class Chick {
+	public Chick() {
+		System.out.println("in constructor");
+	}
+}
