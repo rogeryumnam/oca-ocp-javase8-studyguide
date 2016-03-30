4e2240f3dac74ad23375c712fef146297add1264
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-03-29 22:35:50 -0400

    Chapter 1 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch01/e18_reading_writing_fields/Swan.java
@@ -0,0 +1,21 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch01.e18_reading_writing_fields;
+
+/**
+ * Chapter 1: Java Building Blocks<br/>
+ * Creating Objects<br/>
+ * Reading and Writing Object Fields - page 18
+ * <p>
+ * It’s possible to read and write instance variables directly from the caller. In this example,
+ * a mother swan lays eggs.
+ * <p>
+ * Reading a variable is known as getting it. The class gets numberEggs directly to print it
+ * out. Writing to a variable is known as setting it. This class sets numberEggs to 1.
+ */
+public class Swan {
+	int numberEggs;  // instance variable
+	public static void main(String[] args) {
+		Swan mother = new Swan();
+		mother.numberEggs = 1;  // set variable
+		System.out.println(mother.numberEggs);  // read variable
+	}
+}
