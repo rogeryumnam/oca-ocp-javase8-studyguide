23fab733d30c44fcf12eacfc3bd50f5a2e734bb1
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-04-02 18:26:52 -0400

    Chapter 1 practice

--- src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch01/e24_identifiers/Identifiers.java
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch01/e24_identifiers/Identifiers.java
@@ -3,7 +3,7 @@ package com.jashepherd.studies.java.ocajavase8studyguide.ch01.e24_identifiers;
 /**
  * Chapter 1: Java Building Blocks<br/>
  * Declaring and Initializing Variables<br/>
- * Declaring Multiple Variables - pages 28
+ * Declaring Multiple Variables - page 28
  * <p>
  * There are only three rules to remember for legal identifiers:
  * <ol>
@@ -12,7 +12,7 @@ package com.jashepherd.studies.java.ocajavase8studyguide.ch01.e24_identifiers;
  * <li>You cannot use the same name as a Java reserved word. As you might imagine, a
  * reserved word is a keyword that Java has reserved so that you are not allowed to use it.
  * Remember that Java is case sensitive, so you can use versions of the keywords that only
- * differ in case. Please don’t, though.</li>
+ * differ in case. Please don't, though.</li>
  * </ol>
  */
 public class Identifiers {
