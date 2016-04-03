1c2cd070ce0cd6088cefb2fb8fce63883e27a7fe
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-04-03 16:37:48 -0400

    Chapter 2 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch02/e01_binary_operators/PracticeBinary.java
@@ -0,0 +1,24 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch02.e01_binary_operators;
+
+/**
+ * Chapter 2: Operators and Statements<br/>
+ * Working with Binary Arithmetic Operators<br/>
+ * Arithmetic Operators - pages 54-56
+ * <p>
+ * Pretty basic operator precedence stuff.  % is modulus, also pretty basic.
+ */
+public class PracticeBinary {
+	int x1 = 2 * 5 + 3 * 4 - 8;  // x1 = 14
+	int x2 = 2 * ((5 + 3) * 4 - 8);  // x2 = 48
+
+	public static void main(String... args) {
+		System.out.println(9 / 3);  // Outputs 3
+		System.out.println(9 % 3);  // Outputs 0
+		System.out.println(10 / 3);  // Outputs 3
+		System.out.println(10 % 3);  // Outputs 1
+		System.out.println(11 / 3);  // Outputs 3
+		System.out.println(11 % 3);  // Outputs 2
+		System.out.println(12 / 3);  // Outputs 4
+		System.out.println(12 % 3);  // Outputs 0
+	}
+}
