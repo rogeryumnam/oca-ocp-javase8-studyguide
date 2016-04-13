5929a37a89e28a45fafc87bcd8f634ad59f4fcda
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-04-12 22:29:54 -0400

    Chapter 2 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch02/e14_labels/Labels.java
@@ -0,0 +1,20 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch02.e14_labels;
+
+/**
+ * Chapter 2: Operators and Statements<br/>
+ * Understanding Advanced Flow Control<br/>
+ * Adding Optional Labels - pages 87-88
+ * <p>
+ * In this program the labels are shown but not used
+ */
+public class Labels {
+	static public void main(String args[]) {
+		int[][] myComplexArray = {{5,2,1,3},{3,9,8,9},{5,7,12,7}};
+		OUTER_LOOP: for(int[] mySimpleArray : myComplexArray) {
+			INNER_LOOP: for(int i=0; i<mySimpleArray.length; i++) {
+				System.out.print(mySimpleArray[i]+"\t");
+			}
+			System.out.println();
+		}
+	}
+}
