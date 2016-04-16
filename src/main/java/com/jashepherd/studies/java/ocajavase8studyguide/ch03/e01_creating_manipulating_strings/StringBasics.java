bfc71b8e8e583c8beb52494c32bf24b1fdbd6613
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-04-16 17:01:06 -0400

    Chapter 3 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch03/e01_creating_manipulating_strings/StringBasics.java
@@ -0,0 +1,12 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch03.e01_creating_manipulating_strings;
+
+/**
+ * Chapter 3: Core Java APIs<br>
+ * Creating and Manipulating Strings<br>
+ * pages 102
+ */
+public class StringBasics {
+	// these two statements both create a String object
+	String name = "Fluffy";  // Strings are special - don't have to be instantiated with 'new'
+	String name1 = new String("Fluffy");
+}
