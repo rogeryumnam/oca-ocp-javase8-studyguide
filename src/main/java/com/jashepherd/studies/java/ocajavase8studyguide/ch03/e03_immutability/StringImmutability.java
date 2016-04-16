60aa0fcb634fa3a0e148dfdd2ff84a9abc79feae
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-04-16 17:30:52 -0400

    Chapter 3 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch03/e03_immutability/StringImmutability.java
@@ -0,0 +1,15 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch03.e03_immutability;
+
+/**
+ * Chapter 3: Core Java APIs<br>
+ * Creating and Manipulating Strings<br>
+ * Immutability - pages 104-105
+ */
+public class StringImmutability {
+	public static void main(String[] args) {
+		String s1 = "1";
+		String s2 = s1.concat("2");
+		s2.concat("3");
+		System.out.println(s2);  // outputs 12
+	}
+}
