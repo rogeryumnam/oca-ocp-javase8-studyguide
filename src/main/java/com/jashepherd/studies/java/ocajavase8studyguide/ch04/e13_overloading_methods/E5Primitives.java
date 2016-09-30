bbf202240d69915d1947948f433cb4e8bf72b012
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-09-30 17:04:15 -0400

    Chapter 4 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch04/e13_overloading_methods/E5Primitives.java
@@ -0,0 +1,23 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch04.e13_overloading_methods;
+
+/**
+ * Chapter 4: Methods and Encapsulation<br>
+ * Overloading Methods<br>
+ * Primitives - page 194
+ */
+public class E5Primitives {
+	public static void main(String[] args) {
+		Plane p = new Plane();
+		p.fly(123);   // calls the int version - would call the long version if there was no int version
+		p.fly(123L);  // calls the long version
+	}
+}
+
+class Plane {
+	public void fly(int i) {
+		System.out.print("int ");
+	}
+	public void fly(long l) {
+		System.out.print("long ");
+	}
+}
