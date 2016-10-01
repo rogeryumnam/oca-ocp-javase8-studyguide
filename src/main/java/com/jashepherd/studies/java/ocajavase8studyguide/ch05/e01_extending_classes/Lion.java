0dbab63acc26d8d08014dd8589c0d56d8d0887dd
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-10-01 00:34:52 -0400

    Chapter 5 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch05/e01_extending_classes/Lion.java
@@ -0,0 +1,14 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch05.e01_extending_classes;
+
+/**
+ * Chapter 5: Class Design<br>
+ * Introducing Class Inheritance<br>
+ * Extending a Class - pages 235-236
+ */
+public class Lion extends Animal {
+	private void roar() {
+		System.out.println("The " + getAge() + " year old lion says: Roar!");
+
+//		System.out.println("The " + age + " year old lion says: Roar!");  // DOES NOT COMPILE - age is private in parent class
+	}
+}
