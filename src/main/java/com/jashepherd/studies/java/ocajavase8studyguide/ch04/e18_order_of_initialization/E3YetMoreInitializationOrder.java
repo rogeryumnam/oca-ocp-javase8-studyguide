fa7575f5554cfe1173c7e448df2f976d9dcc15d9
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-11-25 18:32:21 -0500

    Chapter 4 practice

--- src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch04/e18_order_of_initialization/E3YetMoreInitializationOrder.java
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch04/e18_order_of_initialization/E3YetMoreInitializationOrder.java
@@ -9,12 +9,12 @@ package com.jashepherd.studies.java.ocajavase8studyguide.ch04.e18_order_of_initi
  * 2 4 6 8 5
  */
 public class E3YetMoreInitializationOrder {
-	static { add(2); }
+	static { add(2); }                                         // 1st
 	static void add(int num) { System.out.print(num + " "); }
-	E3YetMoreInitializationOrder() { add(5); }
-	static { add(4); }
-	{ add(6); }
+	E3YetMoreInitializationOrder() { add(5); }                 // 5th
+	static { add(4); }                                         // 2nd
+	{ add(6); }                                                // 3rd
 	static { new E3YetMoreInitializationOrder(); }
-	{ add(8); }
+	{ add(8); }                                                // 4th
 	public static void main(String[] args) { }
 }
