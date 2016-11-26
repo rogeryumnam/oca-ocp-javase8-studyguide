ece0d9dab415aec78efe4ff4114b636e28777624
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-11-26 02:52:22 -0500

    Chapter 5 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8studyguide/ch05/e02_class_access_modifiers/Groundhog.java
@@ -0,0 +1,19 @@
+package com.jashepherd.studies.java.ocajavase8studyguide.ch05.e02_class_access_modifiers;
+
+/**
+ * Chapter 5: Class Design<br>
+ * Introducing Class Inheritance<br>
+ * Applying Class Access Modifiers - page 237
+ * <p>
+ * public and default are the only permitted class modifiers.
+ * <p>
+ * A java file may only have one public class and if it does, the filename must match the public class name.
+ * A java file may have any number of default class in it.
+ */
+public class Groundhog extends Rodent{}
+
+// If we wanted to make Rodent class public, it would have to be moved to its own file named Rodent.java
+class Rodent {}
+
+class SomeOtherClass1 {}
+class SomeOtherClass2 {}
