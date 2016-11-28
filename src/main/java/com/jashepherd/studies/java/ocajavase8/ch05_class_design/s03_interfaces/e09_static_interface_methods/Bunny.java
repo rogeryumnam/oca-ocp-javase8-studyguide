84febf46f2c0c996961b0584aec6aa9bc36d2291
Jeremy Shepherd <jeremy.shepherd@outlook.com>
2016-11-27 20:45:47 -0500

    Chapter 5 practice

---
+++ src/main/java/com/jashepherd/studies/java/ocajavase8/ch05_class_design/s03_interfaces/e09_static_interface_methods/Bunny.java
@@ -0,0 +1,17 @@
+package com.jashepherd.studies.java.ocajavase8.ch05_class_design.s03_interfaces.e09_static_interface_methods;
+
+/**
+ * Chapter 5: Class Design<br>
+ * Implementing Interfaces<br>
+ * Static Interface Methods - pages 278-279
+ * <p>
+ * New to Java 8 are static interface methods.  They behave almost exactly like static methods defined in classes except that
+ * they are not inherited.  The interface name must be used to reference the interface static method.
+ */
+public class Bunny implements Hop {
+	public void printDetails() {
+//		System.out.println(getJumpHeight());  // DOES NOT COMPILE - interface name must be used
+		// corrected
+		System.out.println(Hop.getJumpHeight());
+	}
+}
